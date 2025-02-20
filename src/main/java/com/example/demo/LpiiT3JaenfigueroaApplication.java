2a7daa9529957737c921b15a3c68c12c8e117b99
jaenfigueroa <jaenfigueroa03042000@gmail.com>
2025-02-19 20:43:24 -0500

    Creación de clase TblMedico

---
+++ src/main/java/com/example/demo/LpiiT3JaenfigueroaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiT3JaenfigueroaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiT3JaenfigueroaApplication.class, args);
+	}
+
+}
